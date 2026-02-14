322ff87aa4f31dd261ac4821faa5b4e0cad25c02
estarhymar-code <estarhymar@gmail.com>
2026-02-14 12:27:07 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LoginAndLogout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool loginAccess = false;
+
+            string username = "admin";
+            string password = "user";
+            string usernameInput, passwordInput;
+
+            while (loginAccess == false)
+            {
+                Console.Write("Enter username: ");
+                usernameInput = Console.ReadLine();
+                Console.Write("Enter password: ");
+                passwordInput = Console.ReadLine();
+
+                if (usernameInput == username && passwordInput == password)
+                {
+                    loginAccess = true;
+                    Console.WriteLine("Access Granted!");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Credentials, Please Re-enter");
+                }
+            }
+        }
+    }
+}
